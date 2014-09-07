d6dbb92e9ad95909b01c4eea99fe5fa5de07c739
dekokun <shintaro.kurachi@gmail.com>
2014-09-07 22:52:08 +0900

    gulpでのlivereload環境構築

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    shell = require('gulp-shell'),
+    connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '../',
+    livereload: true,
+    port: 8000
+  });
+});
+
+
+gulp.task('make', function() {
+  watch('**/*.markdown')
+    .pipe(shell('make'))
+    .pipe(connect.reload());
+});
+
+
+gulp.task('default', ['connect', 'make']);
